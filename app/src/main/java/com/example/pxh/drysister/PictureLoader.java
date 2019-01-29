0d1875cfcf47d308f9f9fde60c3b0f6ecaf6b650
pxh <peixinhua@unisound.com>
2019-01-29 11:32:00 +0800

    complete first day mission

---
+++ app/src/main/java/com/example/pxh/drysister/PictureLoader.java
@@ -0,0 +1,75 @@
+package com.example.pxh.drysister;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+public class PictureLoader {
+    private ImageView loadImg;
+    private String imgurl;
+    private byte[] picByte;
+
+    Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == 0x123){
+                if (picByte != null){
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(picByte,0,picByte.length);
+                    loadImg.setImageBitmap(bitmap);
+                }
+            }
+        }
+    };
+
+    void load(ImageView loadImg,String imgurl){
+        this.loadImg = loadImg;
+        this.imgurl = imgurl;
+        Drawable drawable = loadImg.getDrawable();
+        if (drawable != null && drawable instanceof BitmapDrawable){
+            Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
+            if (bitmap != null && !bitmap.isRecycled()){
+                bitmap.recycle();
+            }
+        }
+        new Thread(runnable).start();
+    }
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(imgurl);
+                HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setConnectTimeout(10000);
+                if (conn.getResponseCode() == 200){
+                    InputStream in = conn.getInputStream();
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    byte[] bytes = new byte[1024];
+                    int length = -1;
+                    while ((length = in.read(bytes)) != -1){
+                        out.write(bytes,0,length);
+                    }
+                    picByte = out.toByteArray();
+                    in.close();
+                    out.close();
+                    handler.sendEmptyMessage(0x123);
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    };
+}
